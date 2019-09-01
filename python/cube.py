d6e03e492bc7e841a704e36ca6c08071dea9300f
costadaniel <dancosta0601@gmail.com>
2019-09-01 02:24:27 -0300

    Criação da classe do cubo

---
+++ python/cube.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+class Cube:
+  # Construtor padrão
+  def __init__(self, cubeCenter, cubeEdge, uCube):
+    self.C = cubeCenter
+    self.e = cubeEdge
+    self.u = uCube
+    self.color = (0, 139, 204)
+  
+  def verifyColision(self, ray):
+    pass
